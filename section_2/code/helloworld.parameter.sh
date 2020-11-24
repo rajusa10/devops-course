f1ec32ce4c53d0c57de6cabb93dda9df986ffeec
root <root@katello.example.com>
2020-11-24 17:16:00 +0530

    new script for parameter testing

---
+++ section_2/code/helloworld.parameter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Variable $name is passed as a parameter from the Jenkins job
+
+echo "Hello ${name}"
---
+++ section_2/code/helloworld.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Hello World"
+
+pwd ; ls -l
+
+echo "Script ran successfully"
